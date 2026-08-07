8677136375bb672c1fe7dc18c9876253a3b4d812
Igor de Souza Branco <igordesouzabranco@users.noreply.github.com>
2026-08-07 19:49:35 -0300

    Correcoes nos endpoints, no exception handler e adicao de pagina html para teste do swagger

--- src/main/java/igor/branco/origo/handler/GlobalExceptionHandler.java
+++ src/main/java/igor/branco/origo/handler/GlobalExceptionHandler.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 import java.util.stream.Collectors;
 
@@ -39,6 +40,13 @@ public class GlobalExceptionHandler {
                 .build();
 
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+    }
+
+     @ExceptionHandler(NoResourceFoundException.class)
+    public ResponseEntity<ErrorResponse> handleNoResourceFound(NoResourceFoundException ex) {
+        log.warn("Rota não encontrada: {}", ex.getResourcePath());
+        ErrorResponse erro = new ErrorResponse("Recurso não encontrado", 404);
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(erro);
     }
 
     @ExceptionHandler(Exception.class)
